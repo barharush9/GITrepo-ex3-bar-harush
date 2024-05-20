339f64e321fc99251e2d5cb70bbd557f9a849f5d
bar <bar@example.com>
2024-05-20 11:08:56 +0300

    Initial commit

---
+++ BarHarush.java
@@ -0,0 +1,7 @@
+public class BarHarush {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Bar Harush" + i);
+        }
+    }
+}
