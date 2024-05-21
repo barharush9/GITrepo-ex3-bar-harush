979dab490d63a1214d50922a109625694e7e617c
bar <bar@example.com>
2024-05-21 11:29:31 +0300

    change output

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
-            System.out.println("check if its change" + i);
+            System.out.println("check if its change !" + i);
         }
     }
 }
